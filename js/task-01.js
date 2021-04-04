9716762133cc8ac8e9cda86de7785d1a65b069cd
Rostyslav Bordyuh <rostyslavbord@gmail.com>
2021-04-04 18:12:08 +0300

    start

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const ulEl = document.querySelector('#categories');
+console.log(ulEl)
+
+const ulLiEl = document.querySelectorAll('.item');
+console.log(`В списке ${ulLiEl.length} категории`)
+
+const titleEl = document.querySelectorAll('h2');
+console.log(titleEl[0].textContent)
+
+
+
+
+
+
+
