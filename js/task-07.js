d4bd08e35695170348a4d85ff466d2218808d04a
Rostyslav Bordyuh <rostyslavbord@gmail.com>
2021-04-06 15:59:38 +0300

    task 7 done !

--- js/task-07.js
+++ js/task-07.js
@@ -1,16 +1,13 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 textEl.style.fontSize = '16px';
-
-function toScale(x, base ) {
-    const parsed = parseInt(x, base);
-    return parsed;
-}
-console.log(toScale(textEl.style.fontSize, 10));
+const fontSize = parseInt(textEl.style.fontSize, 10);
 
 
 function onInputChange (event) {
-
+    console.log('Инпут изменился!!!')
+    let newFontSize = ((inputEl.value / 100)*2) * fontSize;
+    return textEl.style.fontSize = `${newFontSize}px`;
 }
 
 inputEl.addEventListener('change', onInputChange)
