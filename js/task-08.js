d7212213713416717225178338a067dd0853ce11
Rostyslav Bordyuh <rostyslavbord@gmail.com>
2021-04-06 22:26:46 +0300

    888

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,9 @@
+
 const inputEl = document.querySelector('input')
-
-const btnEl = document.querySelectorAll('[data-action="render"]')
-
+const btnRender = document.querySelector('[data-action="render"]')
 const divBoxes = document.querySelector('#boxes')
 
 inputEl.addEventListener('input', onInputValue)
-
-function onInputValue () {
-    return inputEl.value;
-}
 
 function createBoxes(amount) {
     const divEl = document.createElement('div')
